c1ecc3ad70ff3898e2f3c99b1ea07e8578064569
Mien <2502mien@gmail.com>
2022-04-25 15:49:45 +0700

    add

--- src/components/List.js
+++ src/components/List.js
@@ -1,26 +1,64 @@
-import React from 'react'
+import * as React from 'react';
+import { useEffect } from "react";
+import { db } from "../firebase";
+import { collection, getDocs, deleteDoc, doc, onSnapshot, addDoc } from 'firebase/firestore';
+import { Link } from "react-router-dom";
 
-export default function List() {
+const List = () => {
+  const [todos, setTodos] = React.useState([]);
+  const [message, setMessage] = React.useState('');
+  let unsub = null;
+  useEffect(() => {
+    console.log('List');
+    (async () => {
+      const collectionRef = collection(db, 'todos');
+      unsub = onSnapshot(collectionRef, (snapShot) => {
+        const localTodos = [];
+        console.log("Có sự thay đổi dữ liệu");
+        snapShot.forEach(doc => {
+          localTodos.push({
+            id: doc.id,
+            message: doc.data().message
+          });
+        });
+        setTodos(localTodos);
+      });
+    })();
+  }, []);
+
+  const deleteNote = async (id) => {
+    const docRef = doc(db, 'todos', id);
+    await deleteDoc(docRef);
+  }
+
+  const AddNote = async () => {
+    const collectionRef = collection(db, 'todos');
+    await addDoc(collectionRef, { message });
+    setMessage('');
+  }
+
   return (
-    <div >
-         <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
-        <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
-          <div className="mb-4">
-            <h1 className="text-grey-darkest">Todo List</h1>
-            <div className="flex mt-4">
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Add Todo" />
-              <button className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal">Add</button>
-            </div>
-          </div>
-          <div>
-            <div className="flex mb-4 items-center">
-              <p className="w-full text-grey-darkest">Add another component to Tailwind Components</p>
-              <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">Done</button>
-              <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">Remove</button>
-            </div>
-          </div>
-        </div>
+    <div className="container mx-auto">
+      <div >
+        <input type="text" placeholder="add to do"
+          onChange={(e) => setMessage(e.target.value)} value={message} className="border-2 border-emerald-500" />
+        <button onClick={AddNote}>ADD NOTE</button>
       </div>
+      <ul>
+        {todos.map((todo, index) => (
+
+          <li key={index}>{todo.message}
+
+            <Link to={`/edit?id=${todo.id}`}>Edit</Link>
+            <button onClick={() => {
+              deleteNote(todo.id)
+            }} className="text-red-500">Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
+
+export default List;
