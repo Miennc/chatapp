a56faac9edccf65b80cc4cb101f264e82b5d09ee
longlee1107 <59003423+longlee1107@users.noreply.github.com>
2022-04-25 15:55:49 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 import {
   BrowserRouter,
   Routes,
   Route,
   Link,
 } from "react-router-dom";
-import About from './components/About';
+
 export default function App() {
   return (
     <div>
       <BrowserRouter>
+      <Link to="/login">Sign In</Link>
+      <Link to="/signup">Sign Up</Link>
         <Routes>
-          <Route path="/" element={<About />} >
-        
-          </Route>
+          <Route path="/login" element={<Login />}/>
+          <Route path='/signup' element={<SignUp/>}></Route>
 
         </Routes>
         {/* <Link to={`/about?name=mien&age=20`}>about</Link> */}
